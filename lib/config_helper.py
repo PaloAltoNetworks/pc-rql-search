fc4ba7ff3e7d5a168d0ca74655a35ac17c29d437
Adam <ahamiltonsut@paloaltonetworks.com>
2022-10-07 09:00:38 -0700

    Fixed csv output when adding column

---
+++ lib/config_helper.py
@@ -0,0 +1,19 @@
+import os
+import yaml
+
+
+class ConfigHelper(object):
+    def __init__(self):
+        config = self.read_yml('configs')
+        self.pc_user = config["prisma_cloud"]["username"]
+        self.pc_pass = config["prisma_cloud"]["password"]
+        self.pc_cust = config["prisma_cloud"]["customer_name"]
+        self.pc_api_base = config["prisma_cloud"]["api_base"]
+        self.pc_file_name = config["prisma_cloud"]["filename"]
+        self.pc_rql = config["prisma_cloud"]["rql"]
+
+    @classmethod
+    def read_yml(self, f):
+        yml_path = os.path.join(os.path.dirname(__file__), "../config/%s.yml" % f)
+        with open(yml_path,'r') as stream:
+            return yaml.safe_load(stream)
