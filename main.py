cebeaae2bf1ec0c37b70414da71972117a79c903
Adam <ahamiltonsut@paloaltonetworks.com>
2023-03-03 14:38:07 -0800

    Stability Fix

--- main.py
+++ main.py
@@ -38,14 +38,6 @@ def run_rql_hs():
     csv_headers = ["Resource Name", "Service", "Account", "Region Name", "Last Modified", "Deleted" ]
     dy_headers_all = []
 
-    session.headers.update({"Accept": "text/csv"})
-    res = session.request('POST', '/search/config', payload)
-    session.headers.pop('Accept')
-
-    tz_list = res.text.split('\n')[2].split(',"')[1].split('",')[0].split(' ')[4:] #getting locale info from CSV
-
-    add_column_order = res.text.split('\n')[1].split(',')[7:]
-    add_column_headers = res.text.split('\n')[1].split(',')
 
     #Get headers for CSV and verify RQL
     res = session.request('POST', '/search/config', payload)
@@ -67,10 +59,9 @@ def run_rql_hs():
     filename = config.pc_file_name
     with open(filename, "w", newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        if config.utc:
-            writer.writerow(csv_headers)
-        else:
-            writer.writerow(add_column_headers)
+
+        writer.writerow(csv_headers)
+
 
     #Define CSV Output Function
     def dump_to_csv(res_details, res_data, counter, total_rows):
@@ -80,45 +71,26 @@ def run_rql_hs():
         filename = config.pc_file_name
         with open(filename, "a", newline='', encoding='utf-8') as f:
             writer = object
-            if config.utc:
-                writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL, quotechar = "'")
-            else:
-                writer = csv.writer(f)
-                
+            writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL, quotechar = "'")
             for res in res_data['items']:
                 new_data = []
                 #2023-01-30T19:36:07.921Z
                 # csv_data = [res['name'], res['service'], res['accountName'], res['regionName'], datetime.datetime.fromtimestamp(res['insertTs']/1000.).strftime('%Y-%m-%dT%H:%M:%S'), str(res['deleted']).lower()]
                 
                 # csv_data = [res['name'], res['service'], res['accountName'], res['regionName'], res['insertTs'], str(res['deleted']).lower()]
-                if config.utc ==  True:
-                    #8 hours behind the CSV from the UI for some reason. Add 8 hours (time_offset value is 8 hours in seconds)
-                    name = res['name']
-                    service = res['service']
-                    accountName = res['accountName']
-                    regionName = res['regionName']
-                    time_stamp = datetime.datetime.fromtimestamp(res['insertTs']/1000 + time_offset).isoformat()[:-3]+'Z'
-                    deleted = str(res['deleted']).lower()
-                    csv_data = [f'\"{name}\"', f'\"{service}\"', f'\"{accountName}\"', f'\"{regionName}\"', f'\"{time_stamp}\"', deleted]
-                else:
-                    #convert
-                    tz_code = tz_list[0]
-                    tz_locale = tz_list[1]
-                    to_zone = tz.gettz(tz_code)
-                    utc = datetime.datetime.fromtimestamp(res['insertTs']/1000)
-                    timestamp_with_zone = utc.astimezone(to_zone)
-                    locale_ts = timestamp_with_zone.strftime("%b %d, %Y %I.%M") + ' ' + tz_code + ' ' + tz_locale
 
-                    csv_data = [res['name'], res['service'], res['accountName'], res['regionName'], f'{locale_ts}', str(res['deleted']).lower()]
-
+                #8 hours behind the CSV from the UI for some reason. Add 8 hours (time_offset value is 8 hours in seconds)
+                name = res['name']
+                service = res['service']
+                accountName = res['accountName']
+                regionName = res['regionName']
+                time_stamp = datetime.datetime.fromtimestamp(res['insertTs']/1000 + time_offset).isoformat()[:-3]+'Z'
+                deleted = str(res['deleted']).lower()
+                csv_data = [f'\"{name}\"', f'\"{service}\"', f'\"{accountName}\"', f'\"{regionName}\"', f'\"{time_stamp}\"', deleted]
 
                 if 'dynamicData' in res:
                     headers_order = []
-                    if config.utc:
-                        headers_order = dy_headers
-                    else:
-                        headers_order = add_column_order
-
+                    headers_order = dy_headers
                     for header in headers_order:
                         found = False
 
@@ -132,29 +104,8 @@ def run_rql_hs():
                                 found = True
                         if found == False:
                             new_data.append('None')
-                        
 
                     csv_data.extend(new_data)
-                else:
-                    if config.utc ==  True:
-                        #8 hours behind the CSV from the UI for some reason. Add 8 hours (time_offset value is 8 hours in seconds)
-                        name = res['name']
-                        service = res['service']
-                        accountName = res['accountName']
-                        regionName = res['regionName']
-                        time_stamp = datetime.datetime.fromtimestamp(res['insertTs']/1000 + time_offset).isoformat()[:-3]+'Z'
-                        deleted = str(res['deleted']).lower()
-                        csv_data = [f'\"{name}\"', f'\"{service}\"', f'\"{accountName}\"', f'\"{regionName}\"', f'\"{time_stamp}\"', deleted]
-                    else:
-                        #convert
-                        tz_code = tz_list[0]
-                        tz_locale = tz_list[1]
-                        to_zone = tz.gettz(tz_code)
-                        utc = datetime.datetime.fromtimestamp(res['insertTs']/1000)
-                        timestamp_with_zone = utc.astimezone(to_zone)
-                        locale_ts = timestamp_with_zone.strftime("%b %d, %Y %I.%M") + ' ' + tz_code + ' ' + tz_locale
-
-                        csv_data = [res['name'], res['service'], res['accountName'], res['regionName'], f'{locale_ts}', str(res['deleted']).lower()]
                 
                 writer.writerows([csv_data])
 
