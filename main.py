c6f6a84b948af74b21d7926027e147815a068f50
Adam <ahamiltonsut@paloaltonetworks.com>
2023-11-13 12:53:19 -0800

    API Connection Bug Fix

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from operator import mod
 import lib
-import json
 import csv
-import re
 import datetime
 from dateutil import tz
 from datetime import datetime as dt
@@ -17,8 +15,9 @@ from pcpi import saas_session_manager
 def run_rql_hs():
     #Read Config
     config = lib.ConfigHelper()
-    session_manager = saas_session_manager.SaaSSessionManager('Tenant', config.pc_user, config.pc_pass, "https://" + config.pc_api_base, True, py_logger)
+    session_manager = saas_session_manager.SaaSSessionManager('Tenant', config.pc_user, config.pc_pass, "https://" + config.pc_api_base)
     session = session_manager.create_cspm_session()
+
     rql = config.pc_rql
 
     payload = {
@@ -40,6 +39,7 @@ def run_rql_hs():
 
 
     #Get headers for CSV and verify RQL
+
     res = session.request('POST', '/search/config', payload)
 
     if res.status_code in session.success_status:
